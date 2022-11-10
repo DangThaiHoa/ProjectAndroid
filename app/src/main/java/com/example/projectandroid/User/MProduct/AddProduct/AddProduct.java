fe1349aa071e17c247b671aa16923687886db82b
DangThaiHoa <dangthaihoalol@gmail.com>
2022-11-10 20:45:16 +0700

    Update/Modify AddProduct

--- app/src/main/java/com/example/projectandroid/User/MProduct/AddProduct/AddProduct.java
+++ app/src/main/java/com/example/projectandroid/User/MProduct/AddProduct/AddProduct.java
@@ -6,15 +6,26 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.projectandroid.R;
 
+import kotlin.Unit;
+
 public class AddProduct extends AppCompatActivity {
 
-    ImageView btnBack;
+    String[] itemTypeProduct = {"Đồ Ăn", "Đồ Uống"};
+    AutoCompleteTextView TypeProduct;
+    ArrayAdapter<String> adapterItemTypeProduct;
+
+    ImageView btnBack, ImageProduct;
     Button btnNext;
+    TextView NameProduct, QualityProduct, UnitProduct, PriceProduct;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,16 @@ public class AddProduct extends AppCompatActivity {
 
         btnBack = findViewById(R.id.back_btn);
         btnNext = findViewById(R.id.next_btn);
+        TypeProduct = findViewById(R.id.type_product_addProduct);
+        NameProduct = findViewById(R.id.name_product_addProduct);
+        QualityProduct = findViewById(R.id.quality_product_addProduct);
+        UnitProduct = findViewById(R.id.unit_product_addProduct);
+        PriceProduct = findViewById(R.id.price_product_addProduct);
+        ImageProduct = findViewById(R.id.image_product_addProduct);
+
+        adapterItemTypeProduct = new ArrayAdapter<String>(this, R.layout.list_item_type_product_addproduct,itemTypeProduct);
+
+        TypeProduct.setAdapter(adapterItemTypeProduct);
 
         btnBack();
         btnNext();
@@ -47,6 +68,12 @@ public class AddProduct extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), AddProduct2.class);
+                intent.putExtra("TypeProduct",TypeProduct.getText().toString());
+                intent.putExtra("NameProduct",NameProduct.getText().toString());
+                intent.putExtra("QualityProduct",QualityProduct.getText().toString());
+                intent.putExtra("UnitProduct", UnitProduct.getText().toString());
+                intent.putExtra("PriceProduct",PriceProduct.getText().toString());
+                intent.putExtra("ImageProduct",R.drawable.image_test);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/projectandroid/User/MProduct/AddProduct/AddProduct2.java
+++ app/src/main/java/com/example/projectandroid/User/MProduct/AddProduct/AddProduct2.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.projectandroid.R;
+import com.example.projectandroid.User.Product;
+
+import kotlin.Unit;
 
 public class AddProduct2 extends AppCompatActivity {
 
-    ImageView btnBack;
-    Button btnNext;
+    ImageView btnBack, ImageProduct;
+    Button btnNext, btnCancel;
+    TextView NameProduct, QualityProduct, UnitProduct, PriceProduct,CTypeProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +30,30 @@ public class AddProduct2 extends AppCompatActivity {
         setContentView(R.layout.activity_add_product2);
 
         btnBack = findViewById(R.id.back_btn);
-        btnNext = findViewById(R.id.next_btn);
+        btnNext = findViewById(R.id.comfirm_btn);
+        btnCancel = findViewById(R.id.cancel_btn);
+        CTypeProduct = findViewById(R.id.type_product_CaddProduct);
+        NameProduct = findViewById(R.id.name_product_CaddProduct);
+        QualityProduct = findViewById(R.id.quality_product_CaddProduct);
+        UnitProduct = findViewById(R.id.unit_product_CaddProduct);
+        PriceProduct = findViewById(R.id.price_product_CaddProduct);
+        ImageProduct = findViewById(R.id.image_product_CaddProduct);
+
+        Intent intent = getIntent();
+
+        CTypeProduct.setText(intent.getStringExtra("TypeProduct"));
+        NameProduct.setText(intent.getStringExtra("NameProduct"));
+        QualityProduct.setText(intent.getStringExtra("QualityProduct"));
+        UnitProduct.setText(intent.getStringExtra("UnitProduct"));
+        PriceProduct.setText(intent.getStringExtra("PriceProduct"));
+        ImageProduct.setImageResource(intent.getIntExtra("ImageProduct",0));
+
+
 
         btnBack();
-        btnNext();
+        btnComfim();
+        btnCancel();
+
     }
 
     private void btnBack() {
@@ -40,15 +66,25 @@ public class AddProduct2 extends AppCompatActivity {
         });
     }
 
-    private void btnNext() {
+    private void btnComfim() {
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), AddProduct3.class);
+                Intent intent = new Intent(getApplicationContext(), CompleteAddProduct.class);
                 startActivity(intent);
             }
         });
+    }
 
+    private void btnCancel() {
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Product.class);
+                startActivity(intent);
+            }
+        });
     }
 }
